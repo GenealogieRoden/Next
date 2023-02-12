9e8364f4cd0bce94392a64456fb25d3f4e222480
daanschenkel <20974756+daanschenkel@users.noreply.github.com>
2023-02-12 15:17:36 +0100

    dumb

--- pages/api/getRelatie.js
+++ pages/api/getRelatie.js
@@ -3,19 +3,12 @@ import fetch from "node-fetch";
 export default function handler(req, res) {
   //get query
 
-  const id1 = req.query.id1;
-  const id2 = req.query.id2;
-  if (!req.query.id1)
+  const id1 = req.query.id;
+  if (!req.query.id)
     return res.send({ status: "error", message: "ID1 niet meegegeven" });
 
-  if (!req.query.id2)
-    return res.send({ status: "error", message: "ID2 niet meegegeven" });
-
   var url =
-    "https://GenealogieRodenAPI.daanschenkel.repl.co/api/getRelatie?id1=" +
-    id1 +
-    "&id2=" +
-    id2;
+    "https://GenealogieRodenAPI.daanschenkel.repl.co/api/getRelatie?id=" + id;
 
   fetch(url)
     .then((res) => res.json())
